40a8a3b1b46dd6e013f02e160d06d6af27d28435
Nabil Tallal <itstallal5@gmail.com>
2025-11-03 22:57:51 +0100

    refactor(utils): rename cloudinary and resend utilities for consistency

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-
-dotenv.config();
-
+import { ENV } from './utils/env.js';
 const app = express();
 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 4000;
+const PORT = ENV.PORT || 4000;
 
-if(process.env.NODE_ENV === "production") {
+if(ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
     app.get("*", (_, res) => {
