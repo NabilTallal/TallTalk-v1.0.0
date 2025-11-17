a5ed3c99eea16314e9521e26ea1450328897c238
Nabil Tallal <itstallal5@gmail.com>
2025-11-17 19:08:54 +0100

    feat(utils): align frontend services with backend routes & clean up auth routes

--- Frontend/src/utils/chatService.js
+++ Frontend/src/utils/chatService.js
@@ -1,9 +1,16 @@
 import { axiosInstance } from "./axios.js";
 
 export const chatService = {
-    getMessagesByUserId: (userId) => axiosInstance.get(`/messages/${userId}`),
-    sendMessage: (userId, messageData) => axiosInstance.post(`/messages/send/${userId}`, messageData),
-    deleteMessageForEveryone: (messageId) => axiosInstance.put(`/messages/deleteForAll/${messageId}`),
+    getMessagesByUserId: (userId) =>
+        axiosInstance.get(`/messages/users/${userId}/messages`),
+
+    sendMessage: (userId, messageData) =>
+        axiosInstance.post(`/messages/users/${userId}/messages`, messageData),
+
+    deleteMessageForEveryone: (messageId) =>
+        axiosInstance.put(`/messages/${messageId}/deleteForAll`),
+
     getAllContacts: () => axiosInstance.get("/messages/contacts"),
+
     getMyChatPartners: () => axiosInstance.get("/messages/chats"),
 };
