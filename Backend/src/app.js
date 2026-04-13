eaad37ab6955c6bf2b10e1812ea01e29064fcfed
Nabil Tallal <itstallal5@gmail.com>
2026-04-13 21:44:01 +0200

    Change path to index.html for production environment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -27,7 +27,7 @@ app.use("/api/messages", messageRoutes);
 if (EnvUtil.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/index.html"));
     });
 }
 
