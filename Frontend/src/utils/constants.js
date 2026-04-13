17ef985dfb141d0817afc8a5f35d6b23960cb208
Nabil Tallal <itstallal5@gmail.com>
2026-04-13 22:43:48 +0200

    Simplify BASE_URL assignment in constants.js

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000"
-    : import.meta.env.VITE_API_URL;
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
